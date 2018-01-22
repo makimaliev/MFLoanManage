c6908f2f19b333a10f8a57e28f4d1aced8e645c9
makimaliev <makimaliev@gmail.com>
2018-01-22 13:51:37 +0600

    EventType fixed

--- src/main/java/kg/gov/mf/loan/manage/dao/collection/EventTypeDao.java
+++ src/main/java/kg/gov/mf/loan/manage/dao/collection/EventTypeDao.java
@@ -1,8 +1,8 @@
 package kg.gov.mf.loan.manage.dao.collection;
 
-import javax.swing.event.DocumentEvent.EventType;
 import org.springframework.stereotype.Repository;
 import kg.gov.mf.loan.manage.dao.GenericDaoImpl;
+import kg.gov.mf.loan.manage.model.collection.EventType;
 
 @Repository
 public class EventTypeDao extends GenericDaoImpl<EventType> {
