0def5de9989993220ebb7059d0632ece8efec017
Marlen Akimaliev <makimaliev@gmail.com>
2018-05-16 15:28:26 +0600

    spec search fixed

--- src/main/java/kg/gov/mf/loan/manage/dao/EntitySpecification.java
+++ src/main/java/kg/gov/mf/loan/manage/dao/EntitySpecification.java
@@ -28,20 +28,19 @@ public class EntitySpecification<E> implements Specification<E>{
                 return builder.equal(root.get(criteria.getKey()), criteria.getValue());
             case NEGATION:
                 return builder.notEqual(root.get(criteria.getKey()), criteria.getValue());
-            /*
-                case GREATER_THAN:
-                return builder.greaterThan(root.get(criteria.getKey()), criteria.getValue().toString());
+            case GREATER_THAN:
+                return builder.greaterThan(root.<String> get(criteria.getKey()), criteria.getValue().toString());
             case LESS_THAN:
-                return builder.lessThan(root.get(criteria.getKey()), criteria.getValue().toString());
+                return builder.lessThan(root.<String> get(criteria.getKey()), criteria.getValue().toString());
             case LIKE:
-                return builder.like(root.get(criteria.getKey()), criteria.getValue().toString());
+                //return builder.like(root.<String> get(criteria.getKey()), criteria.getValue().toString());
+                return builder.like(root.<String> get(criteria.getKey()), "%" + criteria.getValue() + "%");
             case STARTS_WITH:
-                return builder.like(root.get(criteria.getKey()), criteria.getValue() + "%");
+                return builder.like(root.<String> get(criteria.getKey()), criteria.getValue() + "%");
             case ENDS_WITH:
-                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue());
-            case CONTAINS:
-                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
-                */
+                return builder.like(root.<String> get(criteria.getKey()), "%" + criteria.getValue());
+            //case CONTAINS:
+                //return builder.like(root.<String> get(criteria.getKey()), "%" + criteria.getValue() + "%");
             default:
                 return null;
         }
