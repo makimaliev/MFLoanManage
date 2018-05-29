3ee8ff1a63a9ae7a05be1b00b0a3b9e45452ab51
Marlen Akimaliev <makimaliev@gmail.com>
2018-05-30 01:10:32 +0600

    debtor view owner typeahead

--- src/main/java/kg/gov/mf/loan/manage/repository/debtor/OwnerRepository.java
+++ src/main/java/kg/gov/mf/loan/manage/repository/debtor/OwnerRepository.java
@@ -1,9 +1,16 @@
 package kg.gov.mf.loan.manage.repository.debtor;
 
 import kg.gov.mf.loan.manage.model.debtor.Owner;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner,Long>{
+    List<Owner> findByNameContains(String q);
+    List<Owner> findByName(String name);
 }
