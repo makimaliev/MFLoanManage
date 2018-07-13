10a665d9ff2477d7b111ecb2aa49af2f5449aa1d
Marlen Akimaliev <makimaliev@gmail.com>
2018-07-13 15:56:20 +0600

    collateral item repo

--- src/main/java/kg/gov/mf/loan/manage/repository/collateral/CollateralItemReposiory.java
+++ src/main/java/kg/gov/mf/loan/manage/repository/collateral/CollateralItemReposiory.java
@@ -4,6 +4,10 @@ import kg.gov.mf.loan.manage.model.collateral.CollateralItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CollateralItemReposiory extends JpaRepository<CollateralItem, Long>{
+
+    List<CollateralItem> findByCollateralAgreementId(long id);
 }
