6ea7f7104f87acd959e2108629b33a78c11bc2b1
Marlen Akimaliev <makimaliev@gmail.com>
2018-08-08 01:55:25 +0200

    loan types base abstact class with child classes

---
+++ src/main/java/kg/gov/mf/loan/manage/model/loan/NormalLoan.java
@@ -0,0 +1,9 @@
+package kg.gov.mf.loan.manage.model.loan;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("1")
+public class NormalLoan extends BaseLoan {
+}
---
+++ src/main/java/kg/gov/mf/loan/manage/model/loan/TrancheeLoan.java
@@ -0,0 +1,8 @@
+package kg.gov.mf.loan.manage.model.loan;
+
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorValue("2")
+public class TrancheeLoan extends BaseLoan {
+}
