c235fac3eef1e0c054def2970769a4f2782e7088
Marlen Akimaliev <makimaliev@gmail.com>
2018-09-21 10:25:53 +0600

    remove querydsl

--- src/main/java/kg/gov/mf/loan/manage/repository/debtor/DebtorRepository.java
+++ src/main/java/kg/gov/mf/loan/manage/repository/debtor/DebtorRepository.java
@@ -1,16 +1,13 @@
 package kg.gov.mf.loan.manage.repository.debtor;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
 import kg.gov.mf.loan.manage.model.debtor.Debtor;
 import kg.gov.mf.loan.manage.model.debtor.Owner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DebtorRepository extends JpaRepository<Debtor, Long>, QueryDslPredicateExecutor {
+public interface DebtorRepository extends JpaRepository<Debtor, Long> {
     Page<Debtor> findByOwner(Owner owner, Pageable pageable);
 }
