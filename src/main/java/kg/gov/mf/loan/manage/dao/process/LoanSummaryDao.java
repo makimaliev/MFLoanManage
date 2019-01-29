17851777c3eb45a3116e317bb5b5f2375b306ca4
Eridan123 <saryguloveridan@gmail.com>
2019-01-29 14:29:34 +0600

    loanSummary getLoanSummariesByOnDateAndLoanId function added

--- src/main/java/kg/gov/mf/loan/manage/dao/process/LoanSummaryDao.java
+++ src/main/java/kg/gov/mf/loan/manage/dao/process/LoanSummaryDao.java
@@ -4,7 +4,9 @@ import kg.gov.mf.loan.manage.dao.GenericDao;
 import kg.gov.mf.loan.manage.model.process.LoanSummary;
 
 import java.util.Date;
+import java.util.List;
 
 public interface LoanSummaryDao extends GenericDao<LoanSummary> {
     LoanSummary getByOnDateAndLoanId(Date onDate, long loanId);
+    List<LoanSummary> getSummariesByOnDateAndLoanId(Date onDate,long loanId);
 }
--- src/main/java/kg/gov/mf/loan/manage/service/process/LoanSummaryService.java
+++ src/main/java/kg/gov/mf/loan/manage/service/process/LoanSummaryService.java
@@ -4,7 +4,9 @@ import kg.gov.mf.loan.manage.service.GenericService;
 import kg.gov.mf.loan.manage.model.process.LoanSummary;
 
 import java.util.Date;
+import java.util.List;
 
 public interface LoanSummaryService extends GenericService<LoanSummary> {
     LoanSummary getByOnDateAndLoanId(Date onDate, long loanId);
+    List<LoanSummary> getSummariesByOnDateAndLoanId(Date onDate, long loanId);
 }
