ee51f39fbc11818e9d5e3d6b4c56b6a15ec933b6
Eridan123 <saryguloveridan@gmail.com>
2019-09-23 12:21:03 +0600

    asset amounts set auto

--- src/main/java/kg/gov/mf/loan/manage/repository/loan/JudgementRepository.java
+++ src/main/java/kg/gov/mf/loan/manage/repository/loan/JudgementRepository.java
@@ -4,6 +4,10 @@ import kg.gov.mf.loan.manage.model.loan.Judgement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JudgementRepository extends JpaRepository<Judgement, Long> {
+
+    List<Judgement> findAllByLoanId(Long loanId);
 }
