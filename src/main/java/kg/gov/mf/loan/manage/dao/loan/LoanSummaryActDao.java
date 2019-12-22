48549295f54d058266aeebb28486dae48897ea17
Eridan123 <saryguloveridan@gmail.com>
2019-12-22 19:25:31 +0600

    check unique reg_num function, loanSummaryActState CRUD, loanSummaryAct list changes

--- src/main/java/kg/gov/mf/loan/manage/dao/loan/LoanSummaryActDao.java
+++ src/main/java/kg/gov/mf/loan/manage/dao/loan/LoanSummaryActDao.java
@@ -3,14 +3,11 @@ package kg.gov.mf.loan.manage.dao.loan;
 import kg.gov.mf.loan.manage.dao.GenericDao;
 import kg.gov.mf.loan.manage.model.debtor.Debtor;
 import kg.gov.mf.loan.manage.model.loan.LoanSummaryAct;
-import kg.gov.mf.loan.manage.util.DateUtils;
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Restrictions;
 
-import java.util.Date;
 import java.util.List;
 
 public interface LoanSummaryActDao extends GenericDao<LoanSummaryAct> {
 
     public List<LoanSummaryAct> getLoanSummaryActByDebtor(Debtor debtor);
+    public boolean isUneque(String reg_number);
 }
--- src/main/java/kg/gov/mf/loan/manage/dao/loan/LoanSummaryActDaoImpl.java
+++ src/main/java/kg/gov/mf/loan/manage/dao/loan/LoanSummaryActDaoImpl.java
@@ -3,23 +3,39 @@ package kg.gov.mf.loan.manage.dao.loan;
 import kg.gov.mf.loan.manage.dao.GenericDaoImpl;
 import kg.gov.mf.loan.manage.model.debtor.Debtor;
 import kg.gov.mf.loan.manage.model.loan.LoanSummaryAct;
-import kg.gov.mf.loan.manage.util.DateUtils;
 import org.hibernate.Criteria;
 import org.hibernate.Hibernate;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository("loanSummaryActDao")
-public class LoanSummaryActDaoImpl extends GenericDaoImpl<LoanSummaryAct> implements LoanSummaryActDao
-{
+public class LoanSummaryActDaoImpl extends GenericDaoImpl<LoanSummaryAct> implements LoanSummaryActDao {
+
+    @PersistenceContext
+    protected EntityManager entityManager;
+
     @Override
     public List<LoanSummaryAct> getLoanSummaryActByDebtor(Debtor debtor)
     {
         Criteria criteria=getCurrentSession().createCriteria(LoanSummaryAct.class);
         criteria.add(Restrictions.eq("debtor", debtor));
         return criteria.list();
+    }
+
+    @Override
+    public boolean isUneque(String reg_number) {
+
+        String checkQuery = "select if(COUNT(*)>0,'false','true')\n" +
+                "from loanSummaryAct where reg_number='"+reg_number+"'";
+        Query query = entityManager.createNativeQuery(checkQuery);
+
+
+        return query.getSingleResult().equals("true");
     }
 
     @Override
